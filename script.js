a37f4cb614e906bd966c3929dc89f3e9bbd29fc1
Davi Perucci <148788050+DaviAbel30@users.noreply.github.com>
2024-11-14 15:08:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
+const wrapper = document.querySelector(".wrapper");
 const carousel = document.querySelector(".carousel");
 const arrowBtns = document.querySelectorAll(".wrapper i ");
 const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const carouselChildrens = [...carousel.children];
 
 
-let isDragging = false, startX, startSrollLeft;
+let isDragging = false, startX, startSrollLeft, timeoutId;
+
+let cardPerView = Math.round(carousel.oppsetWidth / firstCardWidth);
+
+carouselChildrens.slice(-cardPerView).reverse().forEach(card=>{carousel.insertAdjacentHTML("afterbegin",card.outerHTML)});
+
+carouselChildrens.slice(0,-cardPerView).forEach(card=>{carousel.insertAdjacentHTML("beforernd",card.outerHTML)});
+
+
+
 
 arrowBtns.forEach(btn => {
   btn.addEventListener("click", () =>{
@@ -29,6 +40,31 @@ const dragStop = () => {
 
 }
 
+const autoPlay = () =>{
+  if(window.innerWidth < 800)return;
+  timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500 );}
+
+autoPlay();
+
+const infiniteScroll = () =>{
+  if(carousel.scrollLeft === 0){
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
+    carousel.classList.remove("no-transition");
+  }else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth){
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.offsetWidth;
+    carousel.classList.add("no-transition");
+  }
+
+  clearTimeout(timeoutId);
+  if(wrapper.matched(":hover"))autoPlay 
+
+}
+
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
 document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scrool", infiniteScroll);
+wrapper.addEventListener("mouseenter", () =>  clearTimeout(timeoutId));
+carousel.addEventListener("mouseleave", autoPlay);
